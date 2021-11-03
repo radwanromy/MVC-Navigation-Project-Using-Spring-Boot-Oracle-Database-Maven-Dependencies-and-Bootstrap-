518904e033a0b32522d79efbcfbae856de9faf61
radwanromy <radwanromy@gmail.com>
2021-11-03 15:25:40 +0600

    Initialize

---
+++ src/main/java/com/example/demo/SpringMvcProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProject1Application.class, args);
+	}
+
+}
